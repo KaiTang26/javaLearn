bce45ce0fc2a25b88ef6d3736b0c0590abdb74b1
KaiTang26 <16kt16@queensu.ca>
2017-12-20 10:54:02 -0500

    function in java

--- HelloNumbers.java
+++ HelloNumbers.java
@@ -2,9 +2,13 @@ public class HelloNumbers{
     public static void main(String[] args){
         int x =0;
 
+        int sum = 0;
+
         while(x<10){
-            System.out.print(x);
+           
+            System.out.println(sum);
             x+=1;
+            sum +=x;
         }
 
 
