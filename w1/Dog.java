a5249ea5ca1523b4b8ddaeaa2e1aa596f14391d3
KaiTang26 <16kt16@queensu.ca>
2017-12-20 16:08:15 -0500

    instance of class in java

--- w1/Dog.java
+++ w1/Dog.java
@@ -1,5 +1,20 @@
 public class Dog{
-    public static void makeNoice(){
-        System.out.println("Bark!!!");
+
+    public int weightInPounds;
+
+    public Dog(int w){
+        weightInPounds =w;
+    }
+
+    public void makeNoice(){
+        if(weightInPounds<10){
+            System.out.println("yip");
+        } else if(weightInPounds<30){
+            System.out.println("bark");
+
+        }else{
+            System.out.println("wooof");
+        }
+        
     }
 }
--- w1/DogLauncher.java
+++ w1/DogLauncher.java
@@ -1,5 +1,6 @@
 public class DogLauncher{
     public static void main(String[] args){
-        Dog.makeNoice();
+        Dog diandian = new Dog(5);
+        diandian.makeNoice();
     }
 }
