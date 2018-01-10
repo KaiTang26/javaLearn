e89bdd61e29b9ec9f522745343a0c8d34281f5ed
KaiTang26 <16kt16@queensu.ca>
2018-01-09 23:01:05 -0500

    add get method

--- w2/IntList.java
+++ w2/IntList.java
@@ -26,13 +26,44 @@ public class IntList{
         return count;
     }
 
+    public int get(int i){
+        IntList p = this;
+        int index = 0;
+        int answer = p.first;
+
+        while(p != null){
+            if(index == i){
+                answer = p.first;
+                break;
+            }else{
+                index +=1;
+                p = p.rest;
+            }
+        }
+
+        return answer;
+    }
+
+    public int geti(int i){
+
+        if(i == 0){
+            return first;
+        }
+
+        return rest.geti(i-1);
+
+
+    }
+
     public static void main(String[] args){
         IntList L = new IntList(15, null);
-        // L = new IntList(10, L);
-        // L = new IntList(5,L);
+        L = new IntList(10, L);
+        L = new IntList(5,L);
         // System.out.println(L.rest.first);
         System.out.println(L.size());
         System.out.println(L.iterativeSize());
+        System.out.println(L.get(2));
+        System.out.println(L.geti(2));
 
     }
 }
