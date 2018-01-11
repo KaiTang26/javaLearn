8e44359b8f2ed240a09c3e099dedee14a2d614cf
KaiTang26 <16kt16@queensu.ca>
2018-01-10 22:17:21 -0500

    method added

--- w2/SLList.java
+++ w2/SLList.java
@@ -8,6 +8,18 @@ public class SLList {
 
     public static void main(String[] args){
 
-        SLList L = new SLList(10);
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addFirst(5);
+
+        System.out.println(L.getFist());
+    }
+
+    public void addFirst(int x){
+        first = new IntNode(x, first);
+    }
+
+    public int getFist(){
+        return first.item;
     }
 }
