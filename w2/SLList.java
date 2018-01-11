91bf726ee7f619124dab7502099d84d1bb495c61
KaiTang26 <16kt16@queensu.ca>
2018-01-10 22:06:02 -0500

    w2d2

---
+++ w2/SLList.java
@@ -0,0 +1,13 @@
+public class SLList {
+
+    public IntNode first;
+
+    public SLList (int x){
+        first = new IntNode(x, null);
+    }
+
+    public static void main(String[] args){
+
+        SLList L = new SLList(10);
+    }
+}
