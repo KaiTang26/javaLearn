1e56d6e4a556b3a3ffecebe992014e9c6c6f29a6
KaiTang26 <16kt16@queensu.ca>
2018-02-23 14:11:40 -0500

    Q1 done

---
+++ CollectionsExercise/src/Exercise.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Exercise {
+//	Question 1
+	
+//	Question 2 
+	public static Set<String> Question2(List<String> fistName){
+		
+		Set<String> ans = new HashSet<String>(fistName);	
+		
+		return ans;
+	}
+	
+	
+
+}
