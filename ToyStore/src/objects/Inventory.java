a6c1f96c67aab61accc308b13797ceba0ae4dfbd
KaiTang26 <16kt16@queensu.ca>
2018-02-26 14:00:29 -0500

    inventory added

---
+++ ToyStore/src/objects/Inventory.java
@@ -0,0 +1,59 @@
+package objects;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import exceptions.ItemInRecallException;
+import exceptions.NotInItemListException;
+
+public class Inventory {
+	
+	private static HashMap<String, Integer> storage = new HashMap<String, Integer>();
+	private static HashSet<String> recall = new HashSet<String>();
+	private static final Set<String> itemList = new HashSet<String>() {{
+		add("Ball");
+		add("PaintedBlock");
+		add("Train");
+		add("Airplane");
+		
+	}};
+	
+	
+	public static void addItemToStorage(String item, int quantity) throws NotInItemListException 
+	{
+		if(!itemList.contains(item)){
+			throw new NotInItemListException("Item not in list");
+		}else{
+			storage.put(item, quantity);
+		}	
+	}
+	
+	public static void setRecall(String item) throws NotInItemListException{
+		if(!itemList.contains(item)){
+			throw new NotInItemListException("Item not in list");
+		}else{
+			recall.add(item);
+		}	
+		
+	}
+	
+	
+	public static void sellItem(String item, int quantity) throws NotInItemListException, ItemInRecallException{
+		if(!itemList.contains(item)){
+			throw new NotInItemListException("Item not in list");
+			
+		}else if(recall.contains(item)){
+			throw new ItemInRecallException("Item in recall");
+			
+		}else{
+			storage.put(item, storage.get(item)-quantity);
+		}
+		
+	}
+	
+	
+	
+	
+
+}
